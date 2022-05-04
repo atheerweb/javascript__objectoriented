651be32c4da10c4c6e783c9a51933b87467196cf
atheerweb <yousoufmostafamuslim@gmail.com>
2022-05-04 16:14:38 +0200

    prototype inheritence

--- index.js
+++ index.js
@@ -25,3 +25,62 @@ function Circle() {
 
 const another = new Circle();
 
+//stop watch excercise
+
+function StopWatch() {
+  //properties
+  let startTime,
+    endTime,
+    duration,
+    startT = 0;
+
+  //methods
+  this.start = function () {
+    startTime = new Date();
+    startT = startTime.getTime();
+  };
+  this.stop = function () {
+    endTime = new Date();
+    let stopT = endTime.getTime();
+    let seconds = stopT - startT;
+    duration = seconds;
+  };
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
+}
+
+// proper ES6 oop
+class User {
+  constructor(email, userName) {
+     this.email = email;
+    this.userName = userName;
+  }
+}
+function Usser(email, userName) {
+  this.email = email;
+  this.userName = userName;
+}
+// prototype
+User.prototype.login = function () {
+  console.log("heyy");
+};
+User.prototype.logOut = function () {
+  console.log("he yy");
+};
+
+class ahmed extends User {
+    constructor() {
+        super();
+        console.log("hi")
+    }
+}
+
+// constructor function inherteince
+function loly(...args) {
+  Usser.apply(this, args);
+}
+
+const userOne = new ahmed();
